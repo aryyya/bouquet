5e7f00de807ad886782b5dadaf5e5988f8937c77
aryyya <arya@live.ca>
2017-08-29 00:25:08 -0700

    Removed unnecessary / route.

--- server/routes/main-router.js
+++ server/routes/main-router.js
@@ -4,10 +4,6 @@ const router = new require('express').Router()
 
 // ROUTES
 
-router.get('/', (req, res) => {
-  res.end('Hello, world!')
-})
-
 router.post('/greet-me', (req, res) => {
   res.json({
     greeting: `Hello, ${req.body.name}!`
